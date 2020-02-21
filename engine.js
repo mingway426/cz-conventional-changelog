d8c770977aa6a98a4c70144ddff16b69ecfde02b
mingway426 <1146776746@qq.com>
2020-02-21 16:22:40 +0800

    feat: 翻译

--- engine.js
+++ engine.js
@@ -72,7 +72,7 @@ module.exports = function(options) {
         {
           type: 'list',
           name: 'type',
-          message: "Select the type of change that you're committing:",
+          message: "选择 committing 的类型:",
           choices: choices,
           default: options.defaultType
         },
@@ -80,12 +80,10 @@ module.exports = function(options) {
           type: 'input',
           name: 'scope',
           message:
-            'What is the scope of this change (e.g. component or file name): (press enter to skip)',
+            '改动的范围 (e.g. 文件名或者类名): (按 enter 跳过)',
           default: options.defaultScope,
           filter: function(value) {
-            return options.disableScopeLowerCase
-              ? value.trim()
-              : value.trim().toLowerCase();
+            return value.trim().toLowerCase();
           }
         },
         {
@@ -93,129 +91,59 @@ module.exports = function(options) {
           name: 'subject',
           message: function(answers) {
             return (
-              'Write a short, imperative tense description of the change (max ' +
-              maxSummaryLength(options, answers) +
-              ' chars):\n'
+              '这次改动的标题\n'
             );
           },
-          default: options.defaultSubject,
-          validate: function(subject, answers) {
-            var filteredSubject = filterSubject(subject);
-            return filteredSubject.length == 0
-              ? 'subject is required'
-              : filteredSubject.length <= maxSummaryLength(options, answers)
-              ? true
-              : 'Subject length must be less than or equal to ' +
-                maxSummaryLength(options, answers) +
-                ' characters. Current length is ' +
-                filteredSubject.length +
-                ' characters.';
-          },
-          transformer: function(subject, answers) {
-            var filteredSubject = filterSubject(subject);
-            var color =
-              filteredSubject.length <= maxSummaryLength(options, answers)
-                ? chalk.green
-                : chalk.red;
-            return color('(' + filteredSubject.length + ') ' + subject);
-          },
-          filter: function(subject) {
-            return filterSubject(subject);
-          }
         },
         {
           type: 'input',
           name: 'body',
           message:
-            'Provide a longer description of the change: (press enter to skip)\n',
+            '改动的详细描述: (按 enter 跳过)\n',
           default: options.defaultBody
         },
         {
           type: 'confirm',
-          name: 'isBreaking',
-          message: 'Are there any breaking changes?',
-          default: false
-        },
-        {
-          type: 'input',
-          name: 'breakingBody',
-          default: '-',
-          message:
-            'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself:\n',
-          when: function(answers) {
-            return answers.isBreaking && !answers.body;
-          },
-          validate: function(breakingBody, answers) {
-            return (
-              breakingBody.trim().length > 0 ||
-              'Body is required for BREAKING CHANGE'
-            );
-          }
-        },
-        {
-          type: 'input',
-          name: 'breaking',
-          message: 'Describe the breaking changes:\n',
-          when: function(answers) {
-            return answers.isBreaking;
-          }
-        },
-
-        {
-          type: 'confirm',
           name: 'isIssueAffected',
-          message: 'Does this change affect any open issues?',
-          default: options.defaultIssues ? true : false
-        },
-        {
-          type: 'input',
-          name: 'issuesBody',
-          default: '-',
-          message:
-            'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself:\n',
-          when: function(answers) {
-            return (
-              answers.isIssueAffected && !answers.body && !answers.breakingBody
-            );
-          }
-        },
-        {
+          message: '这次改动是否关联了某一个 Issue ？',
+          default: false
+        }, {
           type: 'input',
           name: 'issues',
-          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
+          message: '请添加关联的 Issue (e.g. "fix #123", "re #123".):\n',
           when: function(answers) {
             return answers.isIssueAffected;
-          },
-          default: options.defaultIssues ? options.defaultIssues : undefined
+          }
         }
       ]).then(function(answers) {
         var wrapOptions = {
           trim: true,
-          cut: false,
           newline: '\n',
           indent: '',
           width: options.maxLineWidth
         };
 
         // parentheses are only needed when a scope is present
-        var scope = answers.scope ? '(' + answers.scope + ')' : '';
+        var scope = answers.scope ? '(' + answers.scope.trim() + ')' : '';
 
         // Hard limit this line in the validate
         var head = answers.type + scope + ': ' + answers.subject;
 
         // Wrap these lines at options.maxLineWidth characters
-        var body = answers.body ? wrap(answers.body, wrapOptions) : false;
+        var body = wrap(answers.body, wrapOptions) ;
 
         // Apply breaking change prefix, removing it if already present
         var breaking = answers.breaking ? answers.breaking.trim() : '';
         breaking = breaking
           ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '')
           : '';
-        breaking = breaking ? wrap(breaking, wrapOptions) : false;
+        breaking = wrap(breaking, wrapOptions);
 
-        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : false;
+        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : ' ';
 
-        commit(filter([head, body, breaking, issues]).join('\n\n'));
+        var footer = filter([breaking, issues]).join('\n\n');
+
+        commit(head + '\n\n' + body + '\n\n' + footer);
       });
     }
   };
